fb692b609e6b48323f72b48f351391ded63057ca
Mehboob Alam <mehboobalam786461@gmail.com>
2024-09-05 12:51:19 -0700

    Update main-logo name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 z-10 min-w-full  w-screen bg-dark-1 px-4 py-3 flex gap-3 items-center  justify-between">
       <Link href={'/'} className='flex  gap-2 items-center cursor-pointer'>
-      <Image src={'/icons/main-Logo.svg'} width={30} height={30} alt={'logo'}   />
+      <Image src={'/icons/main-logo.svg'} width={30} height={30} alt={'logo'}   />
       <p className="text-xl font-extrabold  text-white max-sm:hidden">Zoom </p>
       </Link>
 
